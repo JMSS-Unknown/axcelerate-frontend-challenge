fa4757f55b98505c1484f31ca12a8bd5d7497275
Norbert Bayer <31131631+JMSS-Unknown@users.noreply.github.com>
2025-02-18 19:50:25 +1100

    feat: setup vitest and required packages for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
@@ -6,4 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+  },
 })
