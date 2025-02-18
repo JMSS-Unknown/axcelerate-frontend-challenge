3976ac5c2bcc3ff6fc33b9bbca9ff0902cbbf444
Norbert Bayer <31131631+JMSS-Unknown@users.noreply.github.com>
2025-02-18 21:45:03 +1100

    feat: use Vitest globals as that will automatically clean up after each test

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [react(), svgr(), tsconfigPaths()],
   test: {
+    globals: true,
     environment: 'jsdom',
     setupFiles: ['./setupTests.ts'],
   },
