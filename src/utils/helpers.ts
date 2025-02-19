7afdc7ee916fc1a0548be60f080f9eb9ecfd52f4
Norbert Bayer <31131631+JMSS-Unknown@users.noreply.github.com>
2025-02-19 20:30:31 +1100

    doc: add missing param for joinClassNames util

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,8 @@
 /**
  * A helper function to join a list of strings as classnames.
  * It will filter out empty and undefined values
+ *
+ * @param {...*} classes The list of classes to join
  */
 export const joinClassNames = (...classes: (string | undefined)[]) =>
   classes.filter((cls) => Boolean(cls)).join(' ')
